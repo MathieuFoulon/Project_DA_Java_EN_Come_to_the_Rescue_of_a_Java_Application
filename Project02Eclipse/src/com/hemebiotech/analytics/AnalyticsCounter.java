a5b4d3dea49225f7e5223f137ffed6ba6e20ac04
MathieuFoulon <matfoulon@gmail.com>
2023-01-02 16:17:02 +0100

    AnalyticsCounter done, adding call to getSymptomsCounted to Main

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,20 @@
 package com.hemebiotech.analytics;
 
 
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
+    public Map<String, Integer> getSymptomsCounted(List<String> symptomsList) {
 
+        Map<String, Integer> symptomsMapped = new TreeMap<>(); // we use treemap for automatic sorting purpose
+        symptomsList.forEach( symptom ->
+                symptomsMapped.merge(symptom, 1 , Integer::sum) // Integer::sum is equivalent as ( previous , one) -> previous + one
+        );
+
+        return symptomsMapped;
+    }
 
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,13 +1,18 @@
 package com.hemebiotech.analytics;
 
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+
 //      Read the data from text file and returns a list of symptoms
         ReadSymptomDataFromFile dataToRead = new ReadSymptomDataFromFile("Project02Eclipse/Symptoms.txt");
         List<String> symptomsList = dataToRead.getSymptoms();
 
-
+//      Mapping and counting number of occurrences of symptoms to symptoms
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        Map<String, Integer> symptomsMappedAndCounted = analyticsCounter.getSymptomsCounted(symptomsList);
+        System.out.println(symptomsMappedAndCounted);
     }
 }
