a9dd5a78b315d8e8c9662f4acecff18438f3ff85
MathieuFoulon <matfoulon@gmail.com>
2023-01-02 13:40:26 +0100

    Cleaning on Main and AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,46 +1,9 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String[] args) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-		String line = reader.readLine();
 
 
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-				System.out.println("number of rashes: " + rashCount);
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-				System.out.println("number of pupils dilatation: " + pupilCount);
-			}
 
-			line = reader.readLine();	// get another symptom
-		}
-
-		reader.close();
-
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dilated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+//      Read the data from text file and returns a list of symptoms
         ReadSymptomDataFromFile dataToRead = new ReadSymptomDataFromFile("Project02Eclipse/Symptoms.txt");
         List<String> symptomsList = dataToRead.getSymptoms();
-        System.out.println(symptomsList);
+
+
     }
 }
